1e2eb17a38fae67bb174a45dfe0fbd33ff7734d6
Seth Glaser <seth.g@digipen.edu>
2020-07-10 19:18:44 -0700

    Begun the test area.

---
+++ control/Assets/Scripts/BaseHand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHand : MonoBehaviour
+{
+    // Variables
+    public float followSpeed;
+    public Sprite openHand;
+    public Sprite closedHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Follow the mouse harshly
+        transform.position = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        // Update hand
+        
+    }
+}
