93519e49c6316ab3ea89ec90891cbcc36b0b5c70
Seth Glaser <seth.g@digipen.edu>
2020-07-12 04:33:52 -0700

    Finished first build! Also added all assets

---
+++ control/Assets/Scripts/EasySpin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasySpin : MonoBehaviour
+{
+    // Variables
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object
+        transform.Rotate(Vector3.back, speed * Time.deltaTime);
+    }
+}
---
+++ control/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+    // Variables
+    public string level;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
