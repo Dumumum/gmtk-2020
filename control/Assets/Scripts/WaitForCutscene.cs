b9a14cbcb13dae87937a223d8f700f3ff61580f7
Seth Glaser <seth.g@digipen.edu>
2020-07-12 02:30:39 -0700

    Finished functionally?

---
+++ control/Assets/Scripts/WaitForCutscene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForCutscene : MonoBehaviour
+{
+    // Variables
+    public float waitTime;
+    public GameObject curText;
+    public GameObject cutscene;
+    public GameObject smoke;
+    public GameObject car;
+
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // If it is time to start the cutscene
+        if(timer >= waitTime)
+        {
+            curText.SetActive(false);
+            smoke.SetActive(true);
+            smoke.GetComponent<ParticleSystem>().Play();
+            cutscene.SetActive(true);
+
+            // Turn off wheels
+            foreach (WheelJoint2D wheel in car.GetComponents<WheelJoint2D>())
+                wheel.useMotor = false;
+        }
+
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If this is the light, begin the end of the level
+        if (collision.tag == "Light")
+        {
+            curText.SetActive(false);
+            cutscene.SetActive(true);
+        }
+    }
+}
